f90a9b0dc9dfceb7593ac04f4caf6cd4a0328b82
devmasud1 <devmahmud.masu1@gmail.com>
2024-05-22 03:39:49 +0600

    successfully create a product data in database

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express, { Request, Response } from "express";
+import { ProductRoutes } from "./modules/product/product.router";
 const app = express();
+
+//parser
+app.use(express.json());
+
+app.use("/api/products", ProductRoutes);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
---
+++ src/modules/product/product.router.ts
@@ -0,0 +1,9 @@
+import express, { Request, Response } from "express";
+import { Product } from "./product.model";
+import { ProductController } from "./product.controller";
+
+const router = express.Router();
+
+router.post("/", ProductController.createProduct);
+
+export const ProductRoutes = router;
