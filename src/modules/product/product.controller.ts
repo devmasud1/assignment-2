12671d76b228ece91a5e7c346e57547fe2fbe588
devmasud1 <devmahmud.masu1@gmail.com>
2024-05-22 03:51:35 +0600

    implement get all product route

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -12,7 +12,17 @@ const createProduct = async (req: Request, res: Response) => {
     data: result,
   });
 };
+const getAllProduct = async (req: Request, res: Response) => {
+  const result = await ProductServices.getAllProduct();
+
+  res.json({
+    success: true,
+    message: "Product created successfully!",
+    data: result,
+  });
+};
 
 export const ProductController = {
   createProduct,
+  getAllProduct,
 };
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,11 +1,19 @@
 import { TProduct } from "./product.interface";
 import { Product } from "./product.model";
 
+//added product
 const createProduct = async (payLoad: TProduct) => {
   const result = await Product.create(payLoad);
   return result;
 };
 
+//get all product
+const getAllProduct = async () => {
+  const result = await Product.find();
+  return result;
+};
+
 export const ProductServices = {
   createProduct,
+  getAllProduct,
 };
